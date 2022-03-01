5253722be726c4230a19b75a36741a01e9d7bd99
shockz <shockzinfinity@gmail.com>
2022-03-01 21:25:01 +0900

    testing twilio webhook with ngrok

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using ngrokAspNet;
+using Twilio.TwiML;
+
+var builder = WebApplication.CreateBuilder(args);
+if (builder.Environment.IsDevelopment())
+  builder.Services.AddHostedService<TunnelService>();
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/voice", () =>
+{
+  var response = new VoiceResponse();
+  response.Say("Hello World!");
+
+  return Results.Text(response.ToString(), "application/xml");
+});
+
+app.MapPost("/message", () =>
+{
+  var response = new MessagingResponse();
+  response.Message("Hello World!");
+  return Results.Text(response.ToString(), "application/xml");
+});
+
+app.Run();
